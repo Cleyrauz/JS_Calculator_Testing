e0b0f10f080aa5ba9a51cf24823bf1207b98b71f
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-11 19:51:44 +0100

    Tested add function

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,6 +9,14 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
+
+  it('should be able to add 1+4 and get 5', function(){
+    calculator.numberClick(1);
+    calculator.operatorClick('+');
+    calculator.numberClick(4);
+    calculator.operatorClick('=');
+    assert.equal(5, calculator.runningTotal);
+  });
 
 });
