98c73759d2a10d68623bd14b412ad020110e366a
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-12 14:25:06 +0100

    Integration Test: tested display Error on screen if you divide by zero

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -102,8 +102,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('40004600');
   });
 
-  // it('should display error on screen if you divide by zero' function(){
-  //
-  // });
+  it('should display Error on screen if you divide by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Error');
+  });
 
 });
