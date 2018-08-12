ad0f2ef1846a14d2e279d8df422f99126dda5575
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-12 13:23:33 +0100

    Intagration test: Tested multiply numbers

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -42,4 +42,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2');
   });
 
+  it('should be able to multiply numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('63');
+  });
+
 });
