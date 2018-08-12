384a3240c98e73bc38950068a12dfd4eb6b01f1f
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-12 13:21:29 +0100

    Integration test: Tested substract numbers

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -33,4 +33,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4');
   });
 
+  it('should be able to subtract numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  });
+
 });
